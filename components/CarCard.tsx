da117a3778c3cac2363ea1d653514ea1b8ff7244
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-14 16:38:21 +0100

    Added info and button under image

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,6 +13,8 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
+  const [isOpen, setIsOpen] = useState(false);
+
   const carRent = calculateCarRent(city_mpg, year);
 
   return (
@@ -37,6 +39,40 @@ const CarCard = ({ car }: CarCardProps) => {
           priority
           className="object-contain"
         />
+      </div>
+
+      <div className="relative flex w-full mt-2 group">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              alt="Steering Wheel"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" alt="Tire" width={20} height={20} />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gAs.svg" alt="Gas Pump" width={20} height={20} />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
     </div>
   );
