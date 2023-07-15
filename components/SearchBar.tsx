3595cb2ee42b3f07186e9c446c8eafcb7bddf14a
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-15 11:10:56 +0100

    Fixed margin left on search icon

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 import SearchManufacturer from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
-  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+  <button type="submit" className={`z-10 ${otherClasses}`}>
     <Image
       src={"/magnifying-glass.svg"}
       alt={"magnifying glass"}
